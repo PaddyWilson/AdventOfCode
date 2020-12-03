272e5da44bbe974023efd2438c30bae7c6973797
Paddy <paddyw07_93@hotmail.com>
2020-12-03 02:08:58 -0400

    added day 4 files

--- AdventOfCode2020/Day4.cs
+++ AdventOfCode2020/Day4.cs
@@ -5,23 +5,23 @@ using System.Threading;
 
 namespace AdventOfCode2020
 {
-	public class Day0 : BaseDay
+	public class Day4 : BaseDay
 	{
-		public Day0()
+		public Day4()
 		{
-			Day = "0";
+			Day = "4";
 			Answer1 = "0";
 			Answer2 = "0";
 		}
 
 		protected override string Solution1(string[] input)
 		{
-			return "-1";
+			return "0";
 		}
 
 		protected override string Solution2(string[] input)
 		{
-			return "-1";
+			return "0";
 		}
 	}
 }
