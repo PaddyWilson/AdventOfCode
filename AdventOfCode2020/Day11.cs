6201c13f570df84bd273964dfc1766d1d70c0e1f
Paddy <paddyw07_93@hotmail.com>
2020-12-11 18:24:26 -0400

    Day 11 solved

---
+++ AdventOfCode2020/Day11.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace AdventOfCode2020
+{
+	public class Day11 : BaseDay
+	{
+		public Day11()
+		{
+			Day = "11";
+			Answer1 = "2424";
+			Answer2 = "2208";
+		}
+
+		protected override string Solution1(string[] input)
+		{
+			char[,] seats = new char[input.Length + 2, input[0].Length + 2];
+
+			for (int x = 1; x <= input.Length; x++)
+				for (int y = 1; y <= input[x - 1].Length; y++)
+					seats[x, y] = input[x - 1][y - 1];
+
+			while (true)
+			{
+				char[,] seatsTemp = new char[input.Length + 2, input[0].Length + 2];
+				Array.Copy(seats, seatsTemp, seats.Length);
+
+				SearchSeats1(seats, ref seatsTemp, input.Length, input[0].Length);
+				//check it the arrays match
+				if (CheckIfSeatsSame(seats, seatsTemp, input.Length, input[0].Length))
+				{
+					Array.Copy(seatsTemp, seats, seats.Length);
+					break;
+				}
+
+				Array.Copy(seatsTemp, seats, seats.Length);
+				//PrintSeats(seats, input.Length + 2, input[0].Length + 2);
+			}
+
+			//count occupied seats
+			int output = 0;
+			for (int x = 1; x <= input.Length; x++)
+				for (int y = 1; y <= input[x - 1].Length; y++)
+					if (seats[x, y] == '#')
+						output++;
+
+			return output.ToString();
+		}
+
+		private bool CheckIfSeatsSame(char[,] seatsInput, char[,] seatsOutput, int width, int height)
+		{
+			int seatCount = 0;
+			int c = 0;
+			for (int x = 1; x <= width; x++)
+				for (int y = 1; y <= height; y++)
+				{
+					if (seatsInput[x, y] == seatsOutput[x, y])
+						seatCount++;
+					c++;
+				}
+
+			if (seatCount == c)
+				return true;
+			return false;
+		}
+
+		private void SearchSeats1(char[,] seatsInput, ref char[,] seatsOutput, int width, int height)
+		{
+			for (int x = 1; x <= width; x++)
+			{
+				for (int y = 1; y <= height; y++)
+				{
+					int taken = 0;
+					int empty = 0;
+
+					for (int i = -1; i < 2; i++)
+					{
+						if (seatsInput[x + i, y - 1] == 'L')
+							empty++;
+						if (seatsInput[x + i, y - 1] == '#')
+							taken++;
+					}
+
+					for (int i = -1; i < 2; i++)
+					{
+						if (x + i == x) continue;
+						if (seatsInput[x + i, y] == 'L')
+							empty++;
+						if (seatsInput[x + i, y] == '#')
+							taken++;
+					}
+
+					for (int i = -1; i < 2; i++)
+					{
+						if (seatsInput[x + i, y + 1] == 'L')
+							empty++;
+						if (seatsInput[x + i, y + 1] == '#')
+							taken++;
+					}
+
+					if (seatsInput[x, y] == 'L' && taken == 0)
+						seatsOutput[x, y] = '#';
+
+					else if (seatsInput[x, y] == '#' && taken >= 4)
+						seatsOutput[x, y] = 'L';
+
+				}
+			}
+		}
+
+		private void PrintSeats(char[,] seats, int xV, int yV)
+		{
+			for (int x = 0; x < xV; x++)
+			{
+				for (int y = 0; y < yV; y++)
+				{
+					Console.Write(seats[x, y]);
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+		}
+
+		protected override string Solution2(string[] input)
+		{
+			char[,] seats = new char[input.Length + 2, input[0].Length + 2];
+
+			for (int x = 1; x <= input.Length; x++)
+				for (int y = 1; y <= input[x - 1].Length; y++)
+					seats[x, y] = input[x - 1][y - 1];
+
+			while (true)
+			{
+				char[,] seatsTemp = new char[input.Length + 2, input[0].Length + 2];
+				Array.Copy(seats, seatsTemp, seats.Length);
+
+				SearchSeats2(seats, ref seatsTemp, input.Length, input[0].Length);
+				//check it the arrays match
+				if (CheckIfSeatsSame(seats, seatsTemp, input.Length, input[0].Length))
+				{
+					Array.Copy(seatsTemp, seats, seats.Length);
+					break;
+				}
+
+				Array.Copy(seatsTemp, seats, seats.Length);
+				//PrintSeats(seats, input.Length + 2, input[0].Length + 2);
+			}
+
+			//count occupied seats
+			int output = 0;
+			for (int x = 1; x <= input.Length; x++)
+				for (int y = 1; y <= input[x - 1].Length; y++)
+					if (seats[x, y] == '#')
+						output++;
+
+			return output.ToString();
+		}
+
+		private void SearchSeats2(char[,] seatsInput, ref char[,] seatsOutput, int width, int height)
+		{
+			for (int x = 1; x <= width; x++)
+			{
+				for (int y = 1; y <= height; y++)
+				{
+					int taken = 0;
+					int empty = 0;
+
+					//up
+					int count = 1;
+					while (true)
+					{
+						if (y - count == 0) break;
+						if (seatsInput[x, y - count] == 'L')
+						{
+							empty++;
+							break;
+						}
+						if (seatsInput[x, y - count] == '#')
+						{
+							taken++;
+							break;
+						}
+						count++;
+					}
+					//down
+					count = 1;
+					while (true)
+					{
+						if (y + count == height + 1) break;
+						if (seatsInput[x, y + count] == 'L')
+						{
+							empty++;
+							break;
+						}
+						if (seatsInput[x, y + count] == '#')
+						{
+							taken++;
+							break;
+						}
+						count++;
+					}
+
+					//left
+					count = 1;
+					while (true)
+					{
+						if (x - count == 0) break;
+						if (seatsInput[x - count, y] == 'L')
+						{
+							empty++;
+							break;
+						}
+						if (seatsInput[x - count, y] == '#')
+						{
+							taken++;
+							break;
+						}
+						count++;
+					}
+					//right
+					count = 1;
+					while (true)
+					{
+						if (x + count == width + 1) break;
+						if (seatsInput[x + count, y] == 'L')
+						{
+							empty++;
+							break;
+						}
+						if (seatsInput[x + count, y] == '#')
+						{
+							taken++;
+							break;
+						}
+						count++;
+					}
+
+					//left up
+					count = 1;
+					while (true)
+					{
+						if (x - count == 0 || y - count == 0) break;
+						if (seatsInput[x - count, y - count] == 'L')
+						{
+							empty++;
+							break;
+						}
+						if (seatsInput[x - count, y - count] == '#')
+						{
+							taken++;
+							break;
+						}
+						count++;
+					}
+					//right up
+					count = 1;
+					while (true)
+					{
+						if (x - count == 0 || y + count == height + 1) break;
+						if (seatsInput[x - count, y + count] == 'L')
+						{
+							empty++;
+							break;
+						}
+						if (seatsInput[x - count, y + count] == '#')
+						{
+							taken++;
+							break;
+						}
+						count++;
+					}
+
+					//right down
+					count = 1;
+					while (true)
+					{
+						if (x + count == width + 1 || y + count == height + 1) break;
+						if (seatsInput[x + count, y + count] == 'L')
+						{
+							empty++;
+							break;
+						}
+						if (seatsInput[x + count, y + count] == '#')
+						{
+							taken++;
+							break;
+						}
+						count++;
+					}
+					//left down
+					count = 1;
+					while (true)
+					{
+						if (x + count == width + 1 || y - count == 0) break;
+						if (seatsInput[x + count, y - count] == 'L')
+						{
+							empty++;
+							break;
+						}
+						if (seatsInput[x + count, y - count] == '#')
+						{
+							taken++;
+							break;
+						}
+						count++;
+					}
+
+					if (seatsInput[x, y] == 'L' && taken == 0)
+						seatsOutput[x, y] = '#';
+					else if (seatsInput[x, y] == '#' && taken >= 5)
+						seatsOutput[x, y] = 'L';
+
+				}
+			}
+		}
+	}
+}
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -21,10 +21,12 @@ namespace AdventOfCode2020
 				new Day7(),
 				new Day8(),
 				new Day9(),
-				new Day10()
+				new Day10(),
+				new Day11()
 			};
-
-			string[] day7Test1 = new string[]{
+			//day 7 test
+			{
+				string[] day7Test1 = new string[]{
 				"light red bags contain 1 bright white bag, 2 muted yellow bags.",
 				"dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
 				"bright white bags contain 1 shiny gold bag.",
@@ -36,7 +38,7 @@ namespace AdventOfCode2020
 				"dotted black bags contain no other bags.",
 			};
 
-			string[] Day7Test2 = new string[]{
+				string[] Day7Test2 = new string[]{
 				"shiny gold bags contain 2 dark red bags.",
 				"dark red bags contain 2 dark orange bags.",
 				"dark orange bags contain 2 dark yellow bags.",
@@ -46,28 +48,48 @@ namespace AdventOfCode2020
 				"dark violet bags contain no other bags."
 			};
 
+				days[6].AddTestInput(day7Test1, "", "32");
+				days[6].AddTestInput(Day7Test2, "", "126");
+			}
 
-
-			days[6].AddTestInput(day7Test1, "", "32");
-			days[6].AddTestInput(Day7Test2, "", "126");
-
-			string[] Day10Test1 = new string[] { "16", "10", "15", "5", "1", "11", "7", "19", "6", "12", "4" };
-			string[] Day10Test2 = new string[]{"28","33","18","42","31","14","46","20","48","47","24",
+			//day 10 tests
+			{
+				string[] Day10Test1 = new string[] { "16", "10", "15", "5", "1", "11", "7", "19", "6", "12", "4" };
+				string[] Day10Test2 = new string[]{"28","33","18","42","31","14","46","20","48","47","24",
 				"23","49","45","19","38","39","11","1","32","25","35","8",
 				"17","7","9","4","2","34","10","3" };
 
-			string[] Day10Test3 = new string[] { "10","6","4","7","1","5" };
-			string[] Day10Test4 = new string[] { "4","11","7","8","1","6","5" };
-			string[] Day10Test5 = new string[] { "3","1","6","2" };
-			string[] Day10Test6 = new string[] {"17","6","10","5","13","7","1","4","12","11","14" };
-			
-			days[9].AddTestInput(Day10Test5, "", "4");
-			days[9].AddTestInput(Day10Test1, "", "8");
-			days[9].AddTestInput(Day10Test2, "", "19208");
-			days[9].AddTestInput(Day10Test3, "", "4");
-			days[9].AddTestInput(Day10Test4, "", "7");
-			days[9].AddTestInput(Day10Test6, "", "28");
+				string[] Day10Test3 = new string[] { "10", "6", "4", "7", "1", "5" };
+				string[] Day10Test4 = new string[] { "4", "11", "7", "8", "1", "6", "5" };
+				string[] Day10Test5 = new string[] { "3", "1", "6", "2" };
+				string[] Day10Test6 = new string[] { "17", "6", "10", "5", "13", "7", "1", "4", "12", "11", "14" };
 
+				days[9].AddTestInput(Day10Test5, "", "4");
+				days[9].AddTestInput(Day10Test1, "", "8");
+				days[9].AddTestInput(Day10Test2, "", "19208");
+				days[9].AddTestInput(Day10Test3, "", "4");
+				days[9].AddTestInput(Day10Test4, "", "7");
+				days[9].AddTestInput(Day10Test6, "", "28");
+			}
+
+			//day 11 tests
+			{
+				string[] Day11Test1 = new string[] {
+				"L.LL.LL.LL",
+				"LLLLLLL.LL",
+				"L.L.L..L..",
+				"LLLL.LL.LL",
+				"L.LL.LL.LL",
+				"L.LLLLL.LL",
+				"..L.L.....",
+				"LLLLLLLLLL",
+				"L.LLLLLL.L",
+				"L.LLLLL.LL"
+				};
+
+
+				days[10].AddTestInput(Day11Test1, "37", "26");
+			}
 			////run last
 			Console.WriteLine("Running Last");
 			days[days.Count - 1].RunAllSolution1Tests();
