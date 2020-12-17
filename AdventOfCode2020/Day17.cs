ef7cf517b03a2e67b8f2548d08593c3d20b81c07
Paddy <paddyw07_93@hotmail.com>
2020-12-17 10:24:52 -0400

    solution 2 is faster. Using multithreading.

--- AdventOfCode2020/Day17.cs
+++ AdventOfCode2020/Day17.cs
@@ -11,7 +11,7 @@ namespace AdventOfCode2020
 		{
 			Day = "17";
 			Answer1 = "324";
-			Answer2 = "0";
+			Answer2 = "1836";
 		}
 
 		protected override string Solution1(string[] input)
@@ -132,60 +132,25 @@ namespace AdventOfCode2020
 				turnCount++;
 				bool[,,,] temp = new bool[size, size, size, size];
 
+				Thread[] threads = new Thread[size];
 
-				for (int x = 1; x < size - 1; x++)
+				//for (int i = 0; i < threads.Length; i++)
+				//{
+				//	threads[i] = new Thread(() => { ThreadProcess(cubes, ref temp, size, w); });
+				//	threads[i].Start();
+				//}
+
+				for (int w = 1; w < size - 1; w++)
 				{
-					for (int y = 1; y < size - 1; y++)
-					{
-						for (int z = 1; z < size - 1; z++)
-						{
-							for (int w = 1; w < size - 1; w++)
-							{
-								//main area
-								int count = 0;
+					int tempW = w;
+					threads[w] = new Thread(() => { ThreadProcess(cubes, ref temp, size, tempW); });
+					threads[w].Start();
+				}//for w
 
-								for (int x1 = x - 1; x1 < x + 2; x1++)
-								{
-									for (int y1 = y - 1; y1 < y + 2; y1++)
-									{
-										for (int z1 = z - 1; z1 < z + 2; z1++)
-										{
-											for (int w1 = w - 1; w1 < w + 2; w1++)
-											{
-												//dont check it self
-												if (x1 == x && y1 == y && z1 == z && w1 == w)
-													continue;
+				for (int w = 1; w < size - 1; w++)
+					threads[w].Join();
 
-												if (cubes[x1, y1, z1, w1] == true)
-													count++;
-											}
-										}
-									}
-								}
-
-								if ((count == 2 || count == 3) && cubes[x, y, z, w] == true)
-								{
-									temp[x, y, z, w] = true;//stay true
-								}
-								//else if((count == 2 || count == 3) && cubes[x, y, z] == false)
-								//{
-								//	cubes[x, y, z] = false;
-								//}
-								else if (count == 3 && cubes[x, y, z, w] == false)
-								{
-									temp[x, y, z, w] = true;
-								}
-								else
-								{
-									temp[x, y, z, w] = false;
-								}
-
-							}//for w
-						}//for z
-					}//for y
-				}//for x
-
-				Array.Copy(temp, cubes, size * size * size * size);
+					Array.Copy(temp, cubes, size * size * size * size);
 			}//end while
 
 			int output = 0;
@@ -207,6 +172,57 @@ namespace AdventOfCode2020
 			}//for x
 
 			return output.ToString();
+		}
+
+		private void ThreadProcess(bool[,,,] cubes, ref bool[,,,] tempCubes, int size, int w)
+		{
+			for (int x = 1; x < size - 1; x++)
+			{
+				for (int y = 1; y < size - 1; y++)
+				{
+					for (int z = 1; z < size - 1; z++)
+					{
+						//for (int w = 1; w < size - 1; w++)
+						//{
+						//main area
+						int count = 0;
+
+						for (int x1 = x - 1; x1 < x + 2; x1++)
+						{
+							for (int y1 = y - 1; y1 < y + 2; y1++)
+							{
+								for (int z1 = z - 1; z1 < z + 2; z1++)
+								{
+									for (int w1 = w - 1; w1 < w + 2; w1++)
+									{
+										//dont check it self
+										if (x1 == x && y1 == y && z1 == z && w1 == w)
+											continue;
+
+										if (cubes[x1, y1, z1, w1] == true)
+											count++;
+									}
+								}
+							}
+						}
+
+						if ((count == 2 || count == 3) && cubes[x, y, z, w] == true)
+						{
+							tempCubes[x, y, z, w] = true;//stay true
+						}
+						else if (count == 3 && cubes[x, y, z, w] == false)
+						{
+							tempCubes[x, y, z, w] = true;
+						}
+						else
+						{
+							tempCubes[x, y, z, w] = false;
+						}
+
+						//}//for w
+					}//for z
+				}//for y
+			}//for x
 		}
 	}
 }
