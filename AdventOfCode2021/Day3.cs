e99f0dfddddaf4af80b5b8a46c18eb7ed2cc2c4e
Paddy <paddyw07_93@hotmail.com>
2021-12-03 01:52:31 -0400

    Completed 2021 Day 3

---
+++ AdventOfCode2021/Day3.cs
@@ -0,0 +1,119 @@
+﻿using AOC;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2021
+{
+	public class Day3 : BaseDay
+	{
+		public Day3()
+		{
+			Day = "3";
+			Answer1 = "2724524";
+			Answer2 = "2775870";
+		}
+
+		protected override string Solution1(string[] input)
+		{
+			string gammaRate = "";
+			string epsilonRate = "";
+
+			for (int i = 0; i < input[0].Length; i++)
+			{
+				int zero = 0;
+				int one = 0;
+
+				for (int j = 0; j < input.Length; j++)
+				{
+					if (input[j][i] == '1')
+						one++;
+					else
+						zero++;
+				}
+
+				if (one > zero)
+				{
+					gammaRate += "1";
+					epsilonRate += "0";
+				}
+				else
+				{
+					gammaRate += "0";
+					epsilonRate += "1";
+				}
+			}
+
+			int output = Convert.ToInt32(gammaRate, 2) * Convert.ToInt32(epsilonRate, 2);
+
+			return output.ToString();
+		}
+
+		protected override string Solution2(string[] input)
+		{
+			List<string> oxygenList = new List<string>(input);
+			List<string> co2List = new List<string>(input);
+
+			int i = 0;
+			while (oxygenList.Count > 1)
+			{
+				int zero = 0;
+				int one = 0;
+				for (int j = 0; j < oxygenList.Count; j++)
+				{
+					if (oxygenList[j][i] == '1')
+						one++;
+					else
+						zero++;
+				}
+
+				if (one > zero || one == zero)
+				{
+					RemoveItems(oxygenList, i, '0');
+				}
+				else
+				{
+					RemoveItems(oxygenList, i, '1');
+				}
+				i++;
+			}
+
+			i = 0;
+			while (co2List.Count > 1)
+			{
+				int zero = 0;
+				int one = 0;
+				for (int j = 0; j < co2List.Count; j++)
+				{
+					if (co2List[j][i] == '1')
+						one++;
+					else
+						zero++;
+				}
+
+				if (zero < one || one == zero)
+				{
+					RemoveItems(co2List, i, '1');
+				}
+				else
+				{
+					RemoveItems(co2List, i, '0');
+				}
+				i++;
+			}
+
+			int output = Convert.ToInt32(oxygenList[0], 2) * Convert.ToInt32(co2List[0], 2);
+
+			return output.ToString();
+		}
+
+		private void RemoveItems(List<string> input, int binaryIndex, char number)
+		{
+			for (int j = input.Count - 1; j >= 0; j--)
+			{
+				if (input[j][binaryIndex] == number)
+					input.RemoveAt(j);
+			}
+		}
+	}
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -1,57 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using AOC;
 
 namespace AdventOfCode2021
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            BaseDay.InputDir = @"C:\Users\GGGGG\Desktop\Code\AdventOfCode\Input\2021\";
-            BaseDay.Year = "2021";
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BaseDay.InputDir = @"C:\Users\GGGGG\Desktop\Code\AdventOfCode\Input\2021\";
+			BaseDay.Year = "2021";
 
-            List<BaseDay> days = new List<BaseDay>() {
-                new Day0(),
-                new Day1(),
-                new Day2()
-            };
+			List<BaseDay> days = new List<BaseDay>() {
+				new Day0(),
+				new Day1(),
+				new Day2(),
+				new Day3()
+			};
 
-            //tests
-            {
-                //day 6
-                {
-                    //days[6].AddTestInput(File.ReadAllLines(BaseDay.InputDir + "day6-1 test.txt"), "42", "");
-                    //days[6].AddTestInput(File.ReadAllLines(BaseDay.InputDir + "day6-2 test.txt"), "", "4");
-                }
-            }
+			//tests
+			{
+				//day 3
+				{
+					days[3].AddTestInput(File.ReadAllLines(BaseDay.InputDir + "day3test.txt"), "198", "230");
+				}
+			}
 
-            ////run selected
-            //int day = 0;
-            //Console.WriteLine("Running");
-            //days[day].RunAllSolution1Tests();
-            //days[day].RunSolution1();
-            //days[day].RunAllSolution2Tests();
-            //days[day].RunSolution2();
+			////run selected
+			//int day = 0;
+			//Console.WriteLine("Running");
+			//days[day].RunAllSolution1Tests();
+			//days[day].RunSolution1();
+			//days[day].RunAllSolution2Tests();
+			//days[day].RunSolution2();
 
-            ////run last
-            Console.WriteLine("Running Last");
-            days[days.Count - 1].RunAllSolution1Tests();
-            days[days.Count - 1].RunSolution1();
-            days[days.Count - 1].RunAllSolution2Tests();
-            days[days.Count - 1].RunSolution2();
+			////run last
+			Console.WriteLine("Running Last");
+			days[days.Count - 1].RunAllSolution1Tests();
+			days[days.Count - 1].RunSolution1();
+			days[days.Count - 1].RunAllSolution2Tests();
+			days[days.Count - 1].RunSolution2();
 
-            ////run all
-            //Console.WriteLine("Running All");
-            //foreach (var item in days)
-            //{
-            //    item.RunSolution1();
-            //    item.RunAllSolution1Tests();
-            //    item.RunSolution2();
-            //    item.RunAllSolution2Tests();
-            //}
+			////run all
+			Console.WriteLine("Running All");
+			foreach (var item in days)
+			{
+				item.RunAllSolution1Tests();
+				item.RunSolution1();
+				item.RunAllSolution2Tests();
+				item.RunSolution2();
+			}
 
-            Console.ReadKey();
-        }
-    }
+			Console.ReadKey();
+		}
+	}
 }
