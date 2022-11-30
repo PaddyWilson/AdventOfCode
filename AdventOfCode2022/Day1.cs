6cdceb400542cbeabc3049d5dfd73279f0824542
Paddy <paddyw07_93@hotmail.com>
2022-11-30 14:57:32 -0400

    Got ready for 2022

---
+++ AdventOfCode2022/Day1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace AOC
+{
+	//the basic layout of for a new Day
+	public class Day1 : BaseDay
+	{
+		public Day1()
+		{
+			Day = "1";
+			Answer1 = "0";
+			Answer2 = "0";
+		}
+
+		protected override string Solution1(string[] input)
+		{
+			return "-1";
+		}
+
+		protected override string Solution2(string[] input)
+		{
+			return "-1";
+		}
+	}
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AOC;
+
+BaseDay.Year = "2022";
+BaseDay.InputDir = Path.Combine(Directory.GetCurrentDirectory(), "Input", BaseDay.Year + Path.DirectorySeparatorChar.ToString());
+
+List<BaseDay> days = new List<BaseDay>() {
+	new Day0(),
+	new Day1(),
+	//new Day2(),
+	//new Day3(),
+	//new Day4(),
+	//new Day5(),
+	//new Day6(),
+	//new Day7(),
+	//new Day8(),
+	//new Day9(),
+	//new Day10(),
+	//new Day11(),
+	//new Day12(),
+	//new Day13(),
+	//new Day14(),
+	//new Day15(),
+	//new Day16(),
+	//new Day17(),
+	//new Day18(),
+	//new Day19(),
+	//new Day20(),
+	//new Day21(),
+	//new Day22(),
+	//new Day23(),
+	//new Day24(),
+	//new Day25(),
+};
+
+//tests
+{
+	//days[1].AddTestInputFromFile("Day8test.txt", "12", "19");
+}
+
+////run
+int day = days.Count - 1;
+Console.WriteLine("Running");
+days[day].RunAllSolution1Tests();
+days[day].RunSolution1();
+days[day].RunAllSolution2Tests();
+days[day].RunSolution2();
+
+////run all
+Console.WriteLine("Running All");
+foreach (var item in days)
+{
+	item.RunAllSolution1Tests();
+	item.RunSolution1();
+	item.RunAllSolution2Tests();
+	item.RunSolution2();
+}
+
+//windows closes the console window, linux does not
+//keep open on windows
+Helpers.ConsoleReadKeyWindows();
