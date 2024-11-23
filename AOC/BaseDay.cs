033caeab1dd373e30bbe5e9394c8684c73419616
paddy <paddyw07_93@hotmail.com>
2024-11-22 23:39:08 -0400

    BaseDay is now a bit better to look at

--- AOC/BaseDay.cs
+++ AOC/BaseDay.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection.Metadata.Ecma335;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -20,7 +21,7 @@ namespace AOC
 		protected virtual List<string> TestInputAnswers1 { get; private set; }
 		protected virtual List<string> TestInputAnswers2 { get; private set; }
 
-		public void RunSolution1()
+		protected virtual void RunSolution(int solution, Func<string[], string> func, string answer)
 		{
 			if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
     			Console.ForegroundColor = ConsoleColor.White;
@@ -30,128 +31,91 @@ namespace AOC
 			Stopwatch timer = new Stopwatch();
 			ReadInput();
 
+			string output = "";
 			timer.Start();
-			string output = Solution1(Input);
+			output = func(Input);
 			timer.Stop();
-			
-			if ((output == Answer1))
+
+			if (output == answer)
 				Console.BackgroundColor = ConsoleColor.DarkGreen;
 			else
 				Console.BackgroundColor = ConsoleColor.Red;
 
-			Console.Write("Answer 1:{1,16} | Expected:{4,16} | Correct:{2,6} | Run Time:{3,9}", Day, output, (output == Answer1), timer.Elapsed, Answer1, Year);
+			Console.Write("Answer {0,1}:{1,16} | Expected:{2,16} | Correct:{3,6} | Run Time:{4,9}", solution, output, answer, (output == answer), timer.Elapsed);
 			Console.ResetColor();
 			Console.WriteLine();
 		}
 
-		public void RunTestSolution1(string[] input, string answer)
+		public virtual void RunSolution1()
+		{
+			RunSolution(1, Solution1, Answer1);
+		}
+
+		public virtual void RunSolution2()
+		{
+			RunSolution(2, Solution2, Answer2);
+		}
+
+		protected virtual void RunSolutionTest(int solution,  Func<string[], string> func, string[] input, string answer)
 		{
 			if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
     			Console.ForegroundColor = ConsoleColor.White;
-			
+
 			Console.Write("{1} Day {0,2} ", Day, Year);
 
 			Stopwatch timer = new Stopwatch();
+
+			string output = "";
 			timer.Start();
-			string output = Solution1(input);
+			output = func(input);
 			timer.Stop();
-			
-			if ((output == answer))
+
+			if (output == answer)
 				Console.BackgroundColor = ConsoleColor.DarkBlue;
 			else
 				Console.BackgroundColor = ConsoleColor.DarkRed;
 
-			Console.Write("Answer 1:{1,16} | Expected:{4,16} | Correct:{2,6} | Run Time:{3,9} | Test", Day, output, (output == answer), timer.Elapsed, answer, Year);
+			Console.Write("Answer {0,1}:{1,16} | Expected:{2,16} | Correct:{3,6} | Run Time:{4,9}", solution, output, answer, (output == answer), timer.Elapsed);
 			Console.ResetColor();
 			Console.WriteLine();
 		}
 
-		public void RunSolution2()
+		public virtual void RunTestSolution1(string[] input, string answer)
 		{
-			if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-    			Console.ForegroundColor = ConsoleColor.White;
-			
-			Console.Write("{1} Day {0,2} ", Day, Year);
-
-			Stopwatch timer = new Stopwatch();
-			ReadInput();
-			timer.Start();
-			string output = Solution2(Input);
-			timer.Stop();
-			
-			if ((output == Answer2))
-				Console.BackgroundColor = ConsoleColor.DarkGreen;
-			else
-				Console.BackgroundColor = ConsoleColor.Red;
-
-            Console.Write("Answer 2:{1,16} | Expected:{4,16} | Correct:{2,6} | Run Time:{3,9}", Day, output, (output == Answer2), timer.Elapsed, Answer2, Year);
-			Console.ResetColor();
-			Console.WriteLine();
+			RunSolutionTest(1, Solution1, input, answer);
 		}
 
-		public void RunTestSolution2(string[] input, string answer)
+		public virtual void RunTestSolution2(string[] input, string answer)
 		{
-			if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-    			Console.ForegroundColor = ConsoleColor.White;
-			
-			Console.Write("{1} Day {0,2} ", Day, Year);
-
-			Stopwatch timer = new Stopwatch();
-			timer.Start();
-			string output = Solution2(input);
-			timer.Stop();
-			
-			if ((output == answer))
-				Console.BackgroundColor = ConsoleColor.DarkBlue;
-			else
-				Console.BackgroundColor = ConsoleColor.DarkRed;
-
-			Console.Write("Answer 2:{1,16} | Expected:{4,16} | Correct:{2,6} | Run Time:{3,9} | Test", Day, output, (output == answer), timer.Elapsed, answer, Year);
-			Console.ResetColor();
-			Console.WriteLine();
+			RunSolutionTest(2, Solution2, input, answer);
 		}
 
-		private string[] ReadInput()
+		protected virtual string[] ReadInput()
 		{
 			string[] output = null;
 			//windows does not care about caps but linux does
 			if (File.Exists(InputDir + "Day" + Day + ".txt"))
-			{
 				output = File.ReadAllLines(InputDir + "Day" + Day + ".txt");
-			}
 			else if (File.Exists(InputDir + "day" + Day + ".txt"))
-			{
 				output = File.ReadAllLines(InputDir + "day" + Day + ".txt");
-			}
 			else
-			{
 				throw new InvalidDataException("Cant find file \"" + InputDir + "Day" + Day + ".txt" + "\"");
-			}
 			Input = output;
 			return output;
 		}
 
-        protected string[] ReadInput(string filename)
+        protected virtual string[] ReadInput(string filename)
         {
             string[] output = null;
             //windows does not care about caps but linux does
             if (File.Exists(InputDir + filename))
-            {
                 output = File.ReadAllLines(InputDir + filename);
-            }
-            else if (File.Exists(InputDir + filename))
-            {
-                output = File.ReadAllLines(InputDir + filename);
-            }
             else
-            {
                 throw new InvalidDataException("Cant find file \"" + InputDir + filename + "\"");
-            }
-            Input = output;
             return output;
         }
 
-        public void AddTestInput(string[] input, string answer1, string answer2)
+        public virtual void AddTestInput(string[] input, string answer1, string answer2)
 		{
 			if (TestInput == null)
 			{
@@ -165,57 +129,37 @@ namespace AOC
 			TestInputAnswers2.Add(answer2);
 		}
 
-		public void AddTestInputFromFile(string input, string answer1, string answer2)
+		public virtual void AddTestInputFromFile(string filename, string answer1, string answer2)
 		{
-			if (TestInput == null)
-			{
-				TestInput = new List<string[]>();
-				TestInputAnswers1 = new List<string>();
-				TestInputAnswers2 = new List<string>();
-			}
-
-			string[] temp = null;
-			if (File.Exists(InputDir + input))
-			{
-				temp = File.ReadAllLines(InputDir + input);
-			}
-			else if (File.Exists(InputDir + input))
-			{
-				temp = File.ReadAllLines(InputDir + input);
-			}
-			else
-			{
-				throw new InvalidDataException("Cant find file \"" + InputDir + input + "\"");
-			}
-
-			TestInput.Add(temp);
-			TestInputAnswers1.Add(answer1);
-			TestInputAnswers2.Add(answer2);
+			AddTestInput(ReadInput(filename), answer1, answer2);
 		}
 
-		public void RunAllTests()
+		public virtual void RunAllTests()
 		{
 			RunAllSolution1Tests();
 			RunAllSolution2Tests();
 		}
 
-		public void RunAllSolution1Tests()
+		protected void RunAllSolutionTests(List<string[]> testInputs, List<string> testAnswers, 
+			Action<string[], string> func)
 		{
-			if (TestInput != null)
-				for (int i = 0; i < TestInput.Count; i++)
-					if (TestInputAnswers1[i] != "")//no testing answer. don't run
-						RunTestSolution1(TestInput[i], TestInputAnswers1[i]);
+			if (testInputs != null)
+				for (int i = 0; i < testInputs.Count; i++)
+					if (testAnswers[i] != "")//no testing answer. don't run
+						func(testInputs[i], testAnswers[i]);
 		}
 
-		public void RunAllSolution2Tests()
+		public virtual void RunAllSolution1Tests()
 		{
-			if (TestInput != null)
-				for (int i = 0; i < TestInput.Count; i++)
-					if (TestInputAnswers2[i] != "")//no testing answer. don't run
-						RunTestSolution2(TestInput[i], TestInputAnswers2[i]);
+			RunAllSolutionTests(TestInput, TestInputAnswers1, RunTestSolution1);
 		}
 
-		protected abstract string Solution1(string[] input);// { return "BaseDay1"; }
-		protected abstract string Solution2(string[] input);// { return "BaseDay2"; }
+		public virtual void RunAllSolution2Tests()
+		{
+			RunAllSolutionTests(TestInput, TestInputAnswers2, RunTestSolution2);
+		}
+
+		protected abstract string Solution1(string[] input);
+		protected abstract string Solution2(string[] input);
 	}
 }
