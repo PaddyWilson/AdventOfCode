8aea96207423c6da53393603b29c5bede9b141c8
paddy <paddyw07_93@hotmail.com>
2024-12-01 01:18:09 -0400

    day 1 done

--- AdventOfCode2024/Day1.cs
+++ AdventOfCode2024/Day1.cs
@@ -14,20 +14,62 @@ namespace AdventOfCode2024
         public Day1()
         {
             Day = "1";
-            Answer1 = "-1";
-            Answer2 = "-1";
+            Answer1 = "2285373";
+            Answer2 = "21142653";
         }
 
         protected override string Solution1(string[] input)
         {
-            int total = 0;
-            return total.ToString();
+
+            List<int> n1 = new List<int>();
+            List<int> n2 = new List<int>();
+
+            foreach (var item in input)
+            {
+                var t = item.Split("   ");
+                n1.Add(int.Parse(t[0]));
+                n2.Add(int.Parse(t[1]));
+            }
+
+            n1.Sort();
+            n2.Sort();
+
+            int output = 0;
+            for (int i = 0; i < n1.Count; i++)
+            {
+                int t = n1[i] - n2[i];
+                if (t < 0)
+                { t *= -1; }
+                output += t;
+            }
+            return output.ToString();
         }
 
         protected override string Solution2(string[] input)
         {
-            int total = 0;
-            return total.ToString();
+            List<int> n1 = new List<int>();
+            List<int> n2 = new List<int>();
+
+            foreach (var item in input)
+            {
+                var t = item.Split("   ");
+                n1.Add(int.Parse(t[0]));
+                n2.Add(int.Parse(t[1]));
+            }
+
+            int output = 0;
+
+            foreach (var item1 in n1)
+            {
+                int c = 0;
+                foreach (var item2 in n2)
+                {
+                    if(item2 == item1)
+                        c++;
+                }
+                output+= item1*c;
+            }
+            return output.ToString();
         }
     }
 }
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -9,10 +9,14 @@ Runner days = new Runner(Assembly.GetExecutingAssembly());
 
 //tests
 {
-    // days[0].AddTestInput(new string[] {
-    //     "ABBAC" }, "5", "");
-    // days[0].AddTestInput(new string[] {
-    //     "AxBCDDCAxD" }, "", "28");
+    days[0].AddTestInput(new string[] {
+        "3   4",
+        "4   3",
+        "2   5",
+        "1   3",
+        "3   9",
+        "3   3"
+        }, "11", "31");
 
     // days[0].AddTestInputFromFile("everybody_codes_e2024_q01_p1.txt", "1437", "");
     // days[0].AddTestInputFromFile("everybody_codes_e2024_q01_p2.txt", "", "5669");
